ab748583d46f99191e8832113ed4586add7a32b2
The Crystal King <dylanawhite11@gmail.com>
2024-10-25 06:07:21 -0500

    gulpfile created, modules initialized

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
