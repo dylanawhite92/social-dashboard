7bc7765bbc24ae81f11657a96e820c6a971bbf92
The Crystal King <dylanawhite11@gmail.com>
2024-10-25 06:45:12 -0500

    added browsersync package and tasks for browser serve and reload in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
-
+const browsersync = require('browser-sync').create();
 // Sass Task
 let scssTask = () => {
   return src('app/scss/style.scss', { sourcemaps: true })
@@ -23,5 +23,35 @@ let jsTask = () => {
     .pipe(dest('dist', { sourcemaps: '.' }));
 };
 
+//Browsersync
+let browserSyncServe = (cb) => {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
+};
+
+let browserSyncReload = (cb) => {
+  browsersync.reload();
+  cb();
+};
+
+// Watch Sass and JS files and detect changes to reload browser
+let watchTask = () => {
+  watch('*.html', browserSyncReload);
+  watch(
+    ['app/scss/**/*.scss', 'app/**/*.js'],
+    series(scssTask, jsTask, browserSyncReload)
+  );
+};
+
 // Default Gulp Task
-exports.default = series(scssTask, jsTask);
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
